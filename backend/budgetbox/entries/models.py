bb55b9a9efa7aa68bec2be152b941d01d231f632
mscotto <mascotto1995@gmail.com>
2025-08-28 09:55:36 -0500

    entries models created

--- backend/budgetbox/entries/models.py
+++ backend/budgetbox/entries/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class EntryFields(models.Model):
+    amount = models.DecimalField(decimal_places=2, max_digits=20)
+    description = models.TextField(blank=True)
+    merchant_name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    date_paid = models.DateField(blank=True)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='entries')
+
+    def __str__(self):
+        return f"{self.merchant_name}{self.amount}"
--- backend/budgetbox/users/models.py
+++ backend/budgetbox/users/models.py
@@ -12,12 +12,12 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 #         user.set_password(password)
 #         user.save()
 #         return user
-    
+
 #     def create_superuser(self, email, password=None, **extra_fields):
 #         extra_fields.setdefault("is_staff", True)
 #         extra_fields.setdefault("is_superuser", True)
 #         return self.create_user(email, password, **extra_fields)
-    
+
 class BudgetBoxUser(AbstractUser):
     budget_id = models.CharField(max_length=255, unique=False, null=False, blank=False)
     username = None
